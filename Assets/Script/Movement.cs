e2c7f78d4d31a427cba82b831cbf8c5c57ec2fed
trance963 <94801903+trance963@users.noreply.github.com>
2022-02-04 10:43:44 +0400

    v 0.02
    Fixed Jump

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -8,12 +8,18 @@ public class Movement : MonoBehaviour
     public float Speed;
     public float Sensitivity;
     public float Jump;
+    public bool isGrounded;
     public Rigidbody rb;
 
     private void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         rb.freezeRotation = enabled;
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 
     void FixedUpdate()
@@ -39,9 +45,10 @@ public class Movement : MonoBehaviour
             rb.transform.position -= rb.transform.forward * Speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(transform.up * Jump, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 }
