7e092eae6e8c86525565135558bf7abce6879b23
trance963 <94801903+trance963@users.noreply.github.com>
2022-02-08 15:32:10 +0400

    v 0.16

--- Assets/Script/Blob.cs
+++ Assets/Script/Blob.cs
@@ -7,18 +7,15 @@ public class Blob : MonoBehaviour
     public Coal coal;
     public Rigidbody rb;
     public GameObject Fireball;
-    public ParticleSystem Crack;
 
     void Start()
     {
         rb.GetComponent<Rigidbody>();
-        Crack = GetComponent<ParticleSystem>();
     }
 
     private void OnCollisionEnter(Collision other)
     {
         coal.OnCoalDied();
         Fireball.SetActive(false);
-        Crack.Play();
     }
 }
